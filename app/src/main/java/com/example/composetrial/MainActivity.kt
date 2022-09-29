82385728572530d08945f0eb04e102e9e8eb9e12
Poonam Rashingkar <poonam.rashingkar@walmart.com>
2022-09-29 17:39:32 -0400

    added conversation

--- app/src/main/java/com/example/composetrial/MainActivity.kt
+++ app/src/main/java/com/example/composetrial/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,37 +38,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column {
-        Text("Hello $name!")
-        Text(text = "What are you up too, $name")
-    }
-
+fun ImageCard(resId: Int) {
+    Image(
+        painter = painterResource(id = resId), contentDescription = "scenary", modifier = Modifier
+            .size(100.dp)
+            .padding(all = 10.dp)
+    )
 }
 
 @Composable
-fun ImageCard(resId: Int){
-    Image(painter = painterResource(id = resId), contentDescription = "scenary", modifier = Modifier
-        .size(100.dp)
-        .padding(all = 10.dp))
-}
-@Composable
-fun MessageCard(message: Message){
+fun MessageCard(message: Message) {
     Row {
-        ImageCard(resId = R.drawable.scenary )
-        Column {
-            Text(text = message.author)
-            Text(text = message.message)
+        ImageCard(resId = R.drawable.scenary)
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
 
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleMedium
+            )
+           Text(
+                text = message.message,
+                modifier = Modifier.padding(all = 4.dp),
+                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                style = MaterialTheme.typography.bodySmall
+            )
         }
     }
 }
+
 @Composable
-fun Conversation(messages: List<Message>){
-    LazyColumn{
-       items(messages){ message ->
-           MessageCard(message = message)
-       }
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
     }
 }
 
@@ -75,6 +82,6 @@ fun Conversation(messages: List<Message>){
 @Composable
 fun DefaultView() {
     ComposeTrialTheme {
-          Conversation(messages = SampleData.conversationSample)
-        }
+        Conversation(messages = SampleData.conversationSample)
+    }
 }
